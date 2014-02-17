be9c86fbad094e437bca51ed2d895bdf4b862494
Garry Yao <yaojun85@gmail.com>
2014-02-18 01:12:07 +0800

    Separate decorator module, added "from" and "extend" decorator type.

---
+++ mixin/decorator.js
@@ -0,0 +1,170 @@
+/*
+ * TroopJS composer/mixin/decorator
+ * @license MIT http://troopjs.mit-license.org/ © Mikael Karon mailto:mikael@karon.se
+ */
+define([
+	"troopjs-utils/merge",
+	"poly/object"
+], function DecoratorModule(merge) {
+	"use strict";
+
+	var PROTOTYPE = "prototype";
+	var UNDEFINED = undefined;
+	var DECORATE = "decorate";
+	var VALUE = "value";
+	var ARRAY_PROTO = Array[PROTOTYPE];
+	var ARRAY_CONCAT = ARRAY_PROTO.concat;
+	function NOOP() {}
+
+	/**
+	 * Decorator provides customized way to add properties/methods to object created by {@link composer.mixin.factory}.
+	 * Several decorators are provided by this module.
+	 * @class composer.mixin.decorator
+	 * @constructor
+	 * @param {Function} decorate Function that defines how to override the original one.
+	 */
+	function Decorator(decorate) {
+		var descriptor = {};
+
+		/**
+		 * Function that decides what decoration is to make.
+		 * @method decorate
+		 * @param {Object} descriptor The object descriptor that is the current property.
+		 * @param {String} name The property name.
+		 * @param {Object} descriptors List of all property descriptors of the host object.
+		 * @member composer.mixin.decorator
+		 */
+		descriptor[DECORATE] = {
+			"value": decorate
+		};
+
+		// Define properties
+		Object.defineProperties(this, descriptor);
+	}
+
+	// List of decorators.
+	var decorators = {
+
+		/**
+		 * Create a decorator function that is to be run ahead of the original.
+		 * @member composer.mixin.decorator
+		 * @static
+		 * @param {Function} func The decorator function which receives the same arguments as with the original.
+		 * @returns {composer.mixin.decorator}
+		 */
+		"before": function decorateBefore(func) {
+			return new Decorator(function(descriptor) {
+				var next = descriptor[VALUE];
+
+				descriptor[VALUE] = next ? function() {
+					var me = this;
+					return next.apply(me, func.apply(me, arguments) || arguments);
+				} : func;
+
+				return descriptor;
+			});
+		},
+
+		/**
+		 * Create a decorator function that is to be run following the original.
+		 * @member composer.mixin.decorator
+		 * @static
+		 * @param {Function} func The decorator function which receives the return value from the original.
+		 * @returns {composer.mixin.decorator}
+		 */
+		"after": function decorateAfter(func) {
+			return new Decorator(function(descriptor) {
+				var previous = descriptor[VALUE];
+
+				descriptor[VALUE] = previous ? function() {
+					var me = this;
+					var ret = previous.apply(me, arguments);
+					return ret !== UNDEFINED ? func.call(me, ret) : func.apply(me, arguments);
+				} : func;
+
+				return descriptor;
+			});
+		},
+
+		/**
+		 * Create a decorator which is to replace a function property with
+		 *
+		 * @member composer.mixin.decorator
+		 * @static
+		 * @param {Function} func The decorator function which receives the original function as parameter and is supposed to
+		 * return a function that is to replace the original.
+		 * @returns {composer.mixin.decorator}
+		 */
+		"around": function decorateAround(func) {
+			return new Decorator(function(descriptor) {
+				descriptor[VALUE] = func(descriptor[VALUE] || NOOP);
+				return descriptor;
+			});
+		},
+
+		/**
+		 * Create a decorator that is to lend from a particular property from this own or the other class.
+		 *
+		 *  Decorator.from(Foo);
+		 *  Decorator.from("prop");
+		 *  Decorator.from(Foo, "other");
+		 *
+		 * @member composer.mixin.decorator
+		 * @static
+		 * @param {Function} [which] The other class from which to borrow the method, otherwise to borrow from the host class.
+		 * @param {String} [prop] The property name to borrow from, otherwise to borrow the same property name.
+		 * @returns {composer.mixin.decorator}
+		 */
+		"from": function decorateFrom(which, prop) {
+			// Shifting arguments.
+			if (typeof which === 'string') {
+				prop = which;
+				which = UNDEFINED;
+			}
+
+			return new Decorator(function(descriptor, name, descriptors) {
+				// To override a specified property, otherwise simply this property.
+				name = prop || name;
+				// Property is from the the other's prototype, otherwise from own descriptor.
+				descriptor[VALUE] = which ? which[PROTOTYPE][name] : descriptors[name][VALUE];
+				return descriptor;
+			});
+		},
+
+		/**
+		 * Create a decorator upon an existing object property in order to augment it further.
+		 *
+		 * @member composer.mixin.decorator
+		 * @static
+		 * @param {Function|Object...} ext One or more objects to merge into this property, or a function that returns a new object to be used.
+		 * @returns {composer.mixin.decorator}
+		 */
+		"extend": function decorateExtend(ext) {
+			var args = arguments;
+			return new Decorator(function(descriptor, name, descriptors) {
+				var previous = descriptors[name][VALUE];
+				var val;
+
+				if (typeof ext === 'function') {
+					val = ext(previous);
+				}
+				else if (previous !== UNDEFINED) {
+					val = merge.apply({}, ARRAY_CONCAT.apply([previous], args));
+				}
+				descriptor[VALUE] = val;
+				return descriptor;
+			});
+		}
+	};
+
+	// Add decorators as function statics.
+	var descriptors = {};
+	Object.keys(decorators).forEach(function(name) {
+		descriptors[name] = {
+			"value": decorators[name]
+		};
+	});
+	Object.defineProperties(Decorator, descriptors);
+
+	return Decorator;
+});
---
+++ test/factory-test.js
@@ -0,0 +1,159 @@
+/*globals buster:false*/
+buster.testCase("troopjs-composer/mixin/factory", function(run) {
+	"use strict";
+
+	var assert = buster.referee.assert;
+	var refute = buster.referee.refute;
+
+	require([
+		"troopjs-composer/mixin/factory",
+		"troopjs-composer/mixin/decorator"
+	], function(Factory, Decorator) {
+
+		function method1() {}
+
+		var Arith = Factory({
+			"sub": function(a, b) {
+				return a - b;
+			},
+			"sub2": function(a, b) {
+				return b - a;
+			}
+		});
+
+		run({
+			"Factory": {
+				// POSITIVE TESTS
+				"Factory.create": function() {
+					var o = Factory.create(function() {
+						this["prop2"] = "bar";
+					}, {
+						"prop1": "foo",
+						"func1": method1
+					});
+
+					assert.equals(o.prop2, "bar");
+					assert.equals(o.prop1, "foo");
+					assert.equals(o.func1, method1);
+				},
+				"Factory.extend": function() {
+					assert(true);
+				}
+			},
+			"Decorator": {
+
+				"setUp": function() {
+					this.Cal = Factory({
+						name: "cal",
+						supports: {
+							add: true
+						},
+						add: function(a, b) {
+							return a + b;
+						},
+						sub: function(a, b) {
+							return a - b;
+						}
+					});
+				},
+
+				"Decorator.before": function() {
+					var cal = Factory.create(this.Cal, {
+						// Return array that spreads over arguments of original.
+						add: Decorator.before(function(a, b) {
+							assert.equals(a, 1);
+							assert.equals(b, 2);
+							return [1, 3];
+						}),
+						// No return value should not interfere original.
+						sub: Decorator.before(function() { return; })
+					});
+
+					assert.equals(cal.add(1, 2), 4);
+					assert.equals(cal.sub(2, 1), 1);
+				},
+
+				"Decorator.after": function() {
+					var cal = Factory.create(this.Cal, {
+						add: Decorator.after(function(sum) {
+							return sum + 1;
+						}),
+						sub: Decorator.after(function(sub) {
+							assert.equals(sub, 1);
+							return sub - 1;
+						})
+					});
+
+					assert.equals(cal.add(1, 2), 4);
+					assert.equals(cal.sub(2, 1), 0);
+				},
+
+				"Decorator.around": function() {
+					var cal = Factory.create(this.Cal, {
+						add: Decorator.around(function(org) {
+							return function(a, b) {
+								a *= a;
+								b *= b;
+								return org(a,b);
+							}
+						})
+					});
+
+					assert.equals(cal.add(1, 2), 5);
+				},
+
+				"Decorator.from": function() {
+					var cal1 = Factory.create(this.Cal, {
+						sub: Decorator.from(Arith)
+					});
+
+					var cal2 = Factory.create(this.Cal, {
+						sub: Decorator.from(Arith, "sub2")
+					});
+
+					var cal3 = Factory.create(this.Cal, {
+						sub: Decorator.from("add")
+					});
+
+					assert.equals(cal1.sub(2, 1), 1);
+					assert.equals(cal2.sub(2, 1), -1);
+					assert.equals(cal3.sub(2, 1), 3);
+				},
+
+				"Decorator.extend": function() {
+					var Cal = Factory(this.Cal, {
+						supports: Decorator.extend({
+							sub: true
+						})
+					});
+
+					var Cal2 = Cal.extend({
+						mul: function(a, b) { return a * b; },
+						supports: Decorator.extend(function() {
+							return {
+								mul: true,
+								div: false
+							}
+						})
+					});
+
+					var cal1 = Cal();
+					var cal2 = Cal2();
+
+					assert.equals(cal1.supports, {
+						add: true,
+						sub: true
+					});
+
+					assert.equals(cal2.supports, {
+						div: false,
+						mul: true
+					});
+				}
+			}
+		})
+		;
+	})
+	;
+})
+;
